3cc16e5f9df7eb27d58e5b9c49b225516d823f17
Francisco Jose <sousa06092001@gmail.com>
2024-10-06 09:37:15 -0400

    Add SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,5 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import db.DB;
-import model.entities.Department;
-import model.entities.Seller;
-
 public class Program {
 	public static void main(String[] args) {
 		
